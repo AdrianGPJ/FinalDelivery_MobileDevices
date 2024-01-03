8bbaf19f3ba14eb4586025e733c1a3c7e03dfa06
Adrián Ponce <adrianponcexxv@gmail.com>
2024-01-03 18:26:01 +0100

    search bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,82 @@
 import 'dart:convert';
-import 'package:delivery2/model/gifmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+
+import 'model/gifmodel.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Gif {
+  final String name;
+  final String url;
 
+  Gif({required this.name, required this.url});
+}
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Api example',
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('Api example bar'),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController _searchController = TextEditingController();
+  String _searchQuery = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Api example bar'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _searchController,
+              decoration: InputDecoration(
+                labelText: 'Search',
+                prefixIcon: Icon(Icons.search),
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.clear),
+                  onPressed: () {
+                    _searchController.clear();
+                    _updateSearchQuery("");
+                  },
+                ),
+              ),
+              onChanged: (value) {
+                _updateSearchQuery(value);
+              },
+            ),
           ),
-          body: FutureBuilder(
-            future: loadTenorGifs("tonto manel"),
+          FutureBuilder(
+            future:
+                _searchQuery.isNotEmpty ? loadTenorGifs(_searchQuery) : null,
             builder: (context, snapshot) {
-              if (snapshot.hasError) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return CircularProgressIndicator();
+              } else if (snapshot.hasError) {
                 print(snapshot.error);
-                return Text("error: ${snapshot.error}");
+                return Text("Error: ${snapshot.error}");
+              } else if (!snapshot.hasData) {
+                return Text("No data available");
               }
-              final gif = snapshot.data!;
+
+              final gif = snapshot.data as Gif;
+
               return Card(
                 child: Column(
                   children: [
@@ -39,44 +90,22 @@ class MyApp extends StatelessWidget {
                       ),
                     ),
                     Padding(
-                      padding: const EdgeInsets.all(8.0),
+                      padding: const EdgeInsets.all(16.0),
                       child: Text(gif.name),
                     )
                   ],
                 ),
               );
             },
-          )),
+          ),
+        ],
+      ),
     );
   }
+
+  void _updateSearchQuery(String query) {
+    setState(() {
+      _searchQuery = query;
+    });
+  }
 }
-
-// class _GifWidget extends StatelessWidget {
-//   const _GifWidget({
-//     super.key,
-//     required this.gif,
-//   });
-
-//   final Gif gif;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Card(
-//       child: Column(
-//         children: [
-//           Container(
-//             decoration: BoxDecoration(
-//               image: DecorationImage(
-//                 image: NetworkImage(gif.url),
-//               ),
-//             ),
-//           ),
-//           Padding(
-//             padding: const EdgeInsets.all(8.0),
-//             child: Text(gif.name),
-//           )
-//         ],
-//       ),
-//     );
-//   }
-// }
--- lib/model/gifmodel.dart
+++ lib/model/gifmodel.dart
@@ -2,29 +2,6 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
-// class Gif {
-//   String name;
-//   String url;
-
-//   Gif.fromJson(Map<String, dynamic> json)
-//       : name = json["title"],
-//         url = json["images"]["fixed_width_downsampled"]["url"];
-// }
-
-// Future<List<Gif>> loadGifs() async {
-//   final response = await http.get(
-//     Uri.parse(
-//         "https://api.giphy.com/v1/gifs/trending?api_key=rqJcaYvHrheC4rhsOIVP5RpoX6LOQ8GE&limit=2&offset=0&rating=g&bundle=messaging_non_clips"),
-//   );
-//   final json = jsonDecode(response.body);
-//   final jsonGifList = json["data"];
-//   List<Gif> gifList = [];
-//   for (final jsonGif in jsonGifList) {
-//     gifList.add(Gif.fromJson(jsonGif));
-//   }
-//   return gifList;
-// }
-
 class Gif {
   String name;
   String url;
